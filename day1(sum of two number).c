e634a654ebaa57dc38cc70ca1bb7682cf905a7c0
Mayank <mayankpunia83@gmail.com>
2025-09-05 22:16:13 +0530

    Add files via upload

---
+++ day1(sum of two number).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+int a;
+int b;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+printf("the sum of the two number is %d\n",a+b );
+    return 0;
+}
---
+++ day1(sum, difference, product, and quotient).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+int a;
+int b;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+printf("the sum of two number %d\n" ,a+b);
+printf("the diffrence of the two number is %d\n", a-b);
+printf("the product of the two number is %d\n", a*b);
+printf("the quotient of two number is %d\n", a/b);
+    return 0;
+}
