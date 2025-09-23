2950b013c36a8832fca7ccfe0460983f6b825727
Mayank <mayankpunia83@gmail.com>
2025-09-23 11:46:12 +0530

    Add files via upload

---
+++ day26(pattern in line).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Group 1
+    printf("*\n\n");
+
+    // Group 2
+    for (i = 0; i < 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // Group 3
+    for (i = 0; i < 5; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // Group 4
+    for (i = 0; i < 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // Group 5
+    printf("*\n");
+
+    return 0;
+}
